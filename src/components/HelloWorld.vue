34b1f95abcb6f63227d5152fc51a667d4a8f8ece
程伟佳 <chengweijia@192.168.124.35>
2020-08-04 21:21:24 +0800

    更改文案

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}, docher-test...???</h1>
+    <h1>{{ msg }}, love you</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'H',
   props: {
     msg: String
   }
